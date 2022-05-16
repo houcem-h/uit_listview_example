e5a384d5829dad62f61d5744b8a23cc1c7258ebf
houcem <hedhoucem@gmail.com>
2022-05-16 09:01:07 +0100

    create cars_list file

---
+++ lib/cars_list.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import './car.dart';
+
+List<Car> cars = [
+  Car('Aston Martin', 'Virage', Image.asset('images/aston_maritn_virage.jpg')),
+  Car('Audi', 'R8', Image.asset('images/audi_r8.jpg')),
+  Car('Mercedes', 'SLS', Image.asset('images/mercedes_sls.jpg')),
+  Car('BMW', 'i8', Image.asset('images/bmw_i8.jpg')),
+  Car('BMW', 'M4', Image.asset('images/bmw_m4.jpg')),
+  Car('Porshe', 'Panamera', Image.asset('images/porshe_panamera.jpg')),
+  Car('BMW', 'M5', Image.asset('images/bmw_m5.jpg')),
+  Car('Bugatti', 'Chiron', Image.asset('images/bugatti_chiron.jpg')),
+  Car('BMW', 'i8', Image.asset('images/bmw_i8.jpg')),
+  Car('Dodge', 'Challenger', Image.asset('images/dodge_challenger.jpg')),
+  Car('Aston Martin', 'Virage', Image.asset('images/aston_martin_virage.jpg')),
+  Car('Audi', 'R8', Image.asset('images/audi_r8.jpg')),
+  Car('Rolls Royce', 'Ghost', Image.asset('images/rolls_royce_ghost.jpg')),
+  Car('Porshe', '911', Image.asset('images/porshe_911.jpg')),
+  Car('Lamborghini', 'Huracan', Image.asset('images/lamborghini_huracan.jpg')),
+  Car('BMW', 'M5', Image.asset('images/bmw_m5.jpg')),
+  Car('Mercedes', 'GLA', Image.asset('images/mercedes_gla.jpg')),
+  Car('Mercedes', 'SLS', Image.asset('images/mercedes_sls.jpg')),
+  Car('Mustang', 'GT Premium', Image.asset('images/mustang_gt_premium.jpg')),
+  Car('Porshe', '911', Image.asset('images/porshe_911.jpg')),
+  Car('Bugatti', 'Chiron', Image.asset('images/bugatti_chiron.jpg')),
+  Car('Porshe', 'Panamera', Image.asset('images/porshe_panamera.jpg')),
+  Car('BMW', 'M4', Image.asset('images/bmw_m4.jpg')),
+  Car('Rolls Royce', 'Ghost', Image.asset('images/rolls_royce_ghost.jpg')),
+];
